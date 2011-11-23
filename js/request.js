af854ac5e1ef9bce71514ff3438022015c0c56d0
Martin Shaw <martin.shaw@meanbee.com>
2011-11-23 21:21:30 +0000

    Refactored, and added prevents duplicate tweets

--- js/request.js
+++ js/request.js
@@ -2,34 +2,45 @@ $(document).ready(function() {
     loadTweets(10, false);
 });
 
+var activeIds = [];
+
 function loadTweets(qty, bookmark) {
-    if (bookmark == false) {
-        $.ajax({
-            type: "GET",
-            url: "load_recent_tweets.php",
-            data: "next="+qty,
-            success: function (html) {
-                $("#tweets").html(html);
+    updateActiveIds();
+    arguments = {};
+    arguments.type = "GET";
+    arguments.url = "load_recent_tweets.php";
+    arguments.data = "next="+qty;
+    arguments.success = function (html) {
+        //$("#tweets").html(html);
+        addIfDoesntExist($(html));
 
-                var most_recent = $("#tweets .tweet:first-child");
-                var bookmark = most_recent.attr('id');
-                setTimeout("loadTweets(4, " + bookmark + ")", 2000);
-            }
-        });
-    } else {
-        // Schedule the next request
-        $.ajax({
-            type: "GET",
-            url: "load_recent_tweets.php",
-            data: "next="+qty+"&bookmark="+bookmark,
-            success: function (html) {
-                // We have called this before so let's prepend the new tweets. 
-                $("#tweets").prepend(html);
-
-                var most_recent = $("#tweets .tweet:first-child");
-                var bookmark = most_recent.attr('id');
-                setTimeout("loadTweets(4, " + bookmark + ")", 2000);
-            }
-        });
+        var most_recent = $("#tweets .tweet:first-child");
+        var bookmark = most_recent.attr('id');
+        setTimeout("loadTweets(4, " + bookmark + ")", 2000);
     }
+    if (bookmark) {
+        arguments.data += "&bookmark="+bookmark;
+    }
+    $.ajax(arguments);
+}
+
+function updateActiveIds() {
+    activeIds = [];
+    $('#tweets .tweet').each(function(){
+        activeIds[activeIds.length] = $(this).attr('id');
+    });
+}
+
+function addIfDoesntExist(tweets) {
+    tweets.each(function() {
+        var addValue = true;
+        for (var i = 0; i < activeIds.length; i++) {
+            if (activeIds[i] == $(this).attr('id')) {
+                addValue = false; 
+            }
+        }
+        if (addValue) {
+            $("#tweets").prepend($(this));
+        }
+    });
 }
