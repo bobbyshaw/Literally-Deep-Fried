d85b49c057ef2f19f6fcac456abfc2860c09006e
Tom Robertshaw <me@tomrobertshaw.net>
2011-11-23 23:33:09 +0000

    Add about page.

---
+++ about.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="keywords" content="grammar nazi" />
+        <meta name="description" content="An example of using the twitter stream API looking for ridiculous use of the word 'literally'." />
+        <link href="css/normalize.css" rel="stylesheet" type="text/css" media="all" />
+        <link href="css/styles.css" rel="stylesheet" type="text/css" media="all" />
+        <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+        <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js?ver=3.1.3'></script> 
+        <title>Literally deep fried - Top Fools</title>
+    </head>
+
+    <body>
+
+        <nav class="top-navigation">
+            <ul>
+                <li><a href="about" class="active">About</a></li>
+                <li><a href="top-fools">Top Fools</a></li>
+                <li><a href="latest-tweets">Latest Tweets</a></li>
+            </ul>
+        </nav>
+
+        <header>
+            <h1>Literally deep fried</h1>
+        </header>
+
+        <section class="container">
+            <div class="page-title">
+                <h3>About</h3> 
+            </div>
+            <div id="tweets">
+                <p>Completed in a <a href="http://www.riothq.com">Riot</a> hack night, we took our hate for people's misuse of the work "literally" and found them on twitter.  With this we named and shamed all those n00bs that use the word "literally", when really they are just meant to say "I'm a douche".</p>
+            </div>
+        </section>
+
+
+        <footer>
+            <p class="cloud">Imagined by <br/><a href="http://tomrobertshaw.net">Tom Robertshaw</a> <br/>and <br/><a href="#">Martin Shaw</a></p>
+        </footer>
+    </body>
+</html>
