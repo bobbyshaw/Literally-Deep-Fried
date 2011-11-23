f864c970150093ba05d26b56d5c0887f7666e166
Tom Robertshaw <me@tomrobertshaw.net>
2011-11-23 19:56:21 +0000

    Add toHtml function for tweets so that we reduce duplicated code..

--- load_recent_tweets.php
+++ load_recent_tweets.php
@@ -20,59 +20,45 @@ if (isset($_GET['bookmark'])) {
     $latest_tweet = mysql_real_escape_string($_GET['bookmark']);
 
     $query = "SELECT * from tweets WHERE id > '$latest_tweet' order by id desc limit $limit;";
-
     $result = mysql_query($query) or die (mysql_error());
 
     while($tweet = mysql_fetch_array($result)) {
-        // Add markup to tweet where necessary, e.g. links
-        $text = preg_replace( '/(?<!S)((http(s?):\/\/)|(www.))+([\w.1-9\&=#?\-~%;\/]+)/',
-                '<a href="http$3://$4$5">http$3://$4$5</a>', $tweet['text']);
-
-        // Fuzzy-fy the time
-        $time = fuzzy_time($tweet['time']);
-
-        echo <<<END
-        <div id="$tweet[id]" class="tweet">
-            <img src="$tweet[image]" alt="$tweet[name]" title="$tweet[name]" />
-            <div class="tweet-content">
-                <a class="name" href="http://twitter.com/$tweet[screen_name]">$tweet[name]</a>
-                <p>$text</p>
-                <p class="meta-info"><a class="time"
-                        href="http://twitter.com/$tweet[screen_name]/status/$tweet[id]">$time</a></p>
-            </div>
-        </div>
-END;
+        echo toHtml($tweet);
     }
 
 } else {
     // Get latest tweets
 
     $query = "SELECT * from tweets order by id desc limit $limit;";
-
     $result = mysql_query($query) or die (mysql_error());
 
-    while($tweet = mysql_fetch_array($result)) {
-        // Add markup to tweet where necessary, e.g. links
-        $text = preg_replace(
-        '/(?<!S)((http(s?):\/\/)|(www.))+([\w.1-9\&=#?\-~%;\/]+)/',
-        '<a href="http$3://$4$5">http$3://$4$5</a>', $tweet['text']);
-        
-        // Fuzzy-fy the time
-        $time = fuzzy_time($tweet['time']);
-
-        echo <<<END
-        <div id="$tweet[id]" class="tweet">
-            <img src="$tweet[image]" alt="$tweet[name]" title="$tweet[name]" />
-            <div class="tweet-content">
-                <a class="name" href="http://twitter.com/$tweet[screen_name]">$tweet[name]</a>
-                <p>$text</p>
-                <p class="meta-info"><a class="time"
-                    href="http://twitter.com/$tweet[screen_name]/status/$tweet[id]">$time</a></p>
-            </div>
-        </div>
-END;
+    while($tweet = mysql_fetch_array($result)) {        
+        echo toHtml($tweet);
+    }
 
 }
 
+function toHtml($tweet) {
+    
+    // Add markup to tweet where necessary, e.g. links
+    $text = preg_replace( '/(?<!S)((http(s?):\/\/)|(www.))+([\w.1-9\&=#?\-~%;\/]+)/',
+            '<a href="http$3://$4$5">http$3://$4$5</a>', $tweet['text']);
+    
+    // Fuzzy-fy the time
+    $time = fuzzy_time($tweet['time']);
+    
+    $html = <<<END
+    <div id="$tweet[id]" class="tweet">
+        <img src="$tweet[image]" alt="$tweet[name]" title="$tweet[name]" />
+        <div class="tweet-content">
+            <a class="name" href="http://twitter.com/$tweet[screen_name]">$tweet[name]</a>
+            <p>$text</p>
+            <p class="meta-info"><a class="time"
+                href="http://twitter.com/$tweet[screen_name]/status/$tweet[id]">$time</a></p>
+        </div>
+    </div>
+END;
+
+    return $html;
 }
 ?>
