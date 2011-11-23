55b4e0c613e45bafdbd9b64f0d90cc4438b3fa8e
Martin Shaw <martin.shaw@meanbee.com>
2011-11-23 20:31:43 +0000

    Javascript to perform ajax request and update tweets list

--- js/request.js
+++ js/request.js
@@ -1,10 +1,35 @@
 $(document).ready(function() {
-    var getTweets = $.ajax({
-        type: "GET",
-        url: "load_recent_tweets.php",
-        data: "next=10",
-        success: function (html) {
-            $("#tweets").html(html);
-        }
-    });
+    loadTweets(10, false);
 });
+
+function loadTweets(qty, bookmark) {
+    if (bookmark == false) {
+        $.ajax({
+            type: "GET",
+            url: "load_recent_tweets.php",
+            data: "next="+qty,
+            success: function (html) {
+                $("#tweets").html(html);
+
+                var most_recent = $("#tweets .tweet:first-child");
+                var bookmark = most_recent.attr('id');
+                setTimeout("loadTweets(4, " + bookmark + ")", 2000);
+            }
+        });
+    } else {
+        // Schedule the next request
+        $.ajax({
+            type: "GET",
+            url: "load_recent_tweets.php",
+            data: "next="+qty+"&bookmark="+bookmark,
+            success: function (html) {
+                // We have called this before so let's prepend the new tweets. 
+                $("#tweets").prepend(html);
+
+                var most_recent = $("#tweets .tweet:first-child");
+                var bookmark = most_recent.attr('id');
+                setTimeout("loadTweets(4, " + bookmark + ")", 2000);
+            }
+        });
+    }
+}
